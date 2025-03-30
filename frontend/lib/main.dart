faed08dabbe477d6973e3b9b398060b563c5e693
eshaan-vimal <evk2.development@gmail.com>
2025-03-31 00:07:21 +0530

    project initialization

---
+++ frontend/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main ()
+{
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget
+{
+  const MyApp ({super.key});
+
+  @override
+  Widget build (BuildContext context)
+  {
+    return const MaterialApp(
+      title: 'Perplexity Clone',
+      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
